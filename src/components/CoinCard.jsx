501b515c5db60fff644b15670a587db99911302e
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-07-05 01:16:43 +0300

    Style the details page

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -1,38 +1,51 @@
 /* eslint-disable */
 import React from 'react'
 import { useSelector } from 'react-redux';
+import styles from '../styles/coincard.module.css'
+import {AiFillBackward} from 'react-icons/ai'
+import { useNavigate } from 'react-router-dom';
 
 const CoinCard = () => {
   const { selectedCoin, coins } = useSelector((store) => store.coins)
-  console.log(selectedCoin)
+  const navigate = useNavigate()
   const coinArray = coins.filter((coin)=> coin.uuid === selectedCoin)
   const coin = coinArray[0]
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
   });
-  console.log(coin)
 if(!coin){
   return <p>Loading...</p>
 }
  
       return (
-        <div style={{ width: '300px', height: '300px', backgroundColor: `${coin.color + '50'}` }}>
+        <>
+        <AiFillBackward style={{fontSize:'2rem', color:`${coin.color}`}} onClick={()=> navigate('/')}/>
+        <div style={{backgroundColor: `${coin.color + '50'}` }} className={styles.coin}>
+          <div className={styles.icon}>
           <img style={{ width: '30%', height: 'auto' }} src={coin.iconUrl} />
-          <p>Name: {coin.name} <span>{coin.symbol}</span></p>
-          <p>{new Date().toLocaleString()}</p>
-          <p>Rank: #{coin.rank}</p>
-          <p>Current price: {formatter.format(coin.price)}</p>
-          <p> Change: {coin.change}%</p>
-          <p>Market Cap: {formatter.format(coin.marketCap)}</p>
-          <p>Listed Date: {new Date(coin.listedAt * 1000).toLocaleString("en-US",
+          <p>{coin.name} (<span>{coin.symbol})</span></p>
+          <i>As at: {new Date().toLocaleString()}</i>
+          </div>
+          <div className={styles.details}>
+          <p>Rank:<span> #{coin.rank}</span></p>
+          <p>Current price: <span>{formatter.format(coin.price)}</span></p>
+          </div>
+          <div className={styles.details}>
+          <p> Change: <span style={{color: coin.change<0 ? 'red' : 'green'}}>{coin.change}%</span></p>
+          <p>Market Cap: <span>{formatter.format(coin.marketCap)}</span></p>
+          </div>
+        <div className={styles.listed}>
+          <p style={{margin:'5% 0'}}>Listed Date: {new Date(coin.listedAt * 1000).toLocaleString("en-US",
             {
               month: "short",
               day: "2-digit",
               year: "numeric",
             })}</p>
+            </div>
 
         </div>
+        </>
       )
     
 
