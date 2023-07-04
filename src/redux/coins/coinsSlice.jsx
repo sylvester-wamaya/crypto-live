d348cfc3445901e763d8b64d082636bbbdf8a7f5
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-07-04 18:26:39 +0300

    Fetch coins using redux

--- src/redux/coins/coinsSlice.jsx
+++ src/redux/coins/coinsSlice.jsx
@@ -2,15 +2,50 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 
+const url = 'https://api.coinranking.com/v2/coins'
+
 const initialState = {
     coins: [],
     isLoading : false,
+    error: null
 }
+
+export const fetchCoins = createAsyncThunk('coins/fetchCoins', async()=>{
+try
+    {    
+    const res = await fetch(url)
+    const resData = await res.json()
+    //const coinsData = await resData.data.coins
+    return resData.data.coins
+    }catch(err){
+        return err.messege
+    }
+})
+
+
+
 const coinsSlice = createSlice({
     name: 'coins',
     initialState,
     reducers: {
 
+    },
+    extraReducers(build){
+        build
+        .addCase(fetchCoins.pending, (state)=>({
+            ...state,
+            isLoading: true
+        }))
+        .addCase(fetchCoins.fulfilled, (state, action)=>({
+            ...state,
+            coins: action.payload,
+            isLoading: false
+        }))
+        .addCase(fetchCoins.rejected, (state, action)=>({
+            ...state,
+            error: action.error.message,
+            isLoading: false
+        }))
     }
 
 })
