094c79fbbd06f7917a0f1b33577d9254fd7439da
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-07-04 22:04:46 +0300

    Add error handling code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,29 +8,37 @@ import { fetchCoins } from '../redux/coins/coinsSlice'
 
 const Home = () => {
   const [search, setSearch] = useState('')
-   const {coins} = useSelector((store)=>store.coins)
-   const dispatch = useDispatch()
- useEffect(()=>{
-    if(coins.length === 0){
+  const { coins, isLoading, error } = useSelector((store) => store.coins)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (coins.length === 0) {
       dispatch(fetchCoins())
     }
- },[dispatch, coins])
- const filteredCoins = coins.filter((coin)=>coin.name.toLowerCase().includes(search.toLowerCase())||coin.symbol.toLowerCase().includes(search.toLowerCase()))
-
+  }, [dispatch, coins])
+  const filteredCoins = coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()) || coin.symbol.toLowerCase().includes(search.toLowerCase()))
+if(isLoading){
+  return <h2>Loanding...</h2>
+}
+else if(!filteredCoins){
+  return <h3>Oops no Coins!!!</h3>
+}
+else if(error){
+  return <p>{error}</p>
+}
   return (
     <>
-   
-  <input type='text' name='name' placeholder='search a coin...' onChange={(e)=>setSearch(e.target.value)}/>
-    <div className='grid'>
-   
-      { 
-filteredCoins.map((coin)=>(
-  <CoinBanner color={coin.color} icon={coin.iconUrl} name={coin.name} symbol={coin.symbol} id={coin.uuid} key={coin.uuid}/> 
-    
-    
-))
- }
-    </div> 
+
+      <input type='text' name='name' placeholder='search a coin...' onChange={(e) => setSearch(e.target.value)} />
+      <div className='grid'>
+
+        {
+          filteredCoins.map((coin) => (
+            <CoinBanner color={coin.color} icon={coin.iconUrl} name={coin.name} symbol={coin.symbol} id={coin.uuid} key={coin.uuid} />
+
+
+          ))
+        }
+      </div>
     </>
   )
 }
