61338e18a9d9511a4e6137a7d04e4ff3cfcba83c
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-07-04 22:48:16 +0300

    Style the coin banners

--- src/components/CoinBanner.jsx
+++ src/components/CoinBanner.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { viewDetail } from '../redux/coins/coinsSlice'
+import { RxExternalLink } from 'react-icons/rx'
 
 const CoinBanner = ({color, icon, name, id}) => {
 const dispatch = useDispatch()
   return (
    
-      <Link to='detail' onClick={()=>dispatch(viewDetail(id)) } style={{width:'100%', height:'50vw', backgroundColor:`${color + '50'}`, backgroundImage:`url(${icon})`,backgroundSize:'contain'}}>{name}</Link>
+      <Link to='detail' onClick={()=>dispatch(viewDetail(id)) } style={{ backgroundColor:`${color + '50'}`, backgroundImage:`url(${icon})`}} className='banner'><div className='ban-content'><p>{name}</p><span><RxExternalLink/></span></div></Link>
    
   )
 }
