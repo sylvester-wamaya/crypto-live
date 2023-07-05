e4d986d321ac12ac85693489bf8f59e9a431980c
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-07-05 20:44:34 +0300

    Add tests for coinsSlice

---
+++ src/__tests__/coinsSlice.test.jsx
@@ -0,0 +1,15 @@
+
+import coinsReducer, { fetchCoins } from "../redux/coins/coinsSlice"
+ let state = null
+test('test coins reducer for pending', ()=>{    
+    state = coinsReducer(undefined, fetchCoins.pending())
+    expect(state.isLoading).toBe(true)
+})
+test('test coins reducer for fulfilled', ()=>{   
+    state = coinsReducer(undefined, fetchCoins.fulfilled())
+    expect(state.isLoading).toBe(false)
+})
+test('test coins reducer for fulfilled', ()=>{    
+    state = coinsReducer(undefined, fetchCoins.rejected())
+    expect(state.isLoading).toBe(false)
+})
