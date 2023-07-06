71991188fe49b2a8f6e01977f10d47a2ef71d689
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-07-06 20:25:04 +0300

    Add the summary

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,30 @@
-
+/*eslint-disable */
 import { useEffect, useState } from 'react'
 import CoinBanner from './CoinBanner'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchCoins } from '../redux/coins/coinsSlice'
+import { fetchCoins, filterCoins } from '../redux/coins/coinsSlice'
 import { formatter } from './CoinCard'
 
 
 
 const Home = () => {
-  const [search, setSearch] = useState('')
-  const { coins, isLoading, error, summary } = useSelector((store) => store.coins)
-  console.log(summary)
+  const { coins, isLoading, error, filteredCoins, summary } = useSelector((store) => store.coins)
   const dispatch = useDispatch()
   useEffect(() => {
     if (coins.length === 0) {
       dispatch(fetchCoins())
     }
   }, [dispatch, coins])
-  const filteredCoins = coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()) || coin.symbol.toLowerCase().includes(search.toLowerCase()))
+
+
+  function sumCoin(){let sumCoins = 0
+  for(let i = 0; i<state.filteredCoins.length; i++){
+       sumCoins += 1
+   }
+      state.summary.totalCoins = sumCoins
+ }
+  console.log(summary)
+
 if(isLoading){
   return <h2>Loanding...</h2>
 }
@@ -30,13 +37,15 @@ if(error){
     {
       Object.keys(summary).length !== 0 ? (
       <div className='summary'>
-        <p>Total coins: <span>{summary.total.toLocaleString("en-US")}</span></p>
+        <p>Total coins: <span>{summary.totalCoins > 0 ? summary.totalCoins.toLocaleString("en-US") : 0}</span></p>
         <p>Total Market cap: <span>{formatter.format(summary.totalMarketCap)}</span></p>
       </div>
       ) : ''
 }
 <div className='input'>
-      <input type='text' name='name' placeholder='Type to search...' onChange={(e) => setSearch(e.target.value)} />
+      <input type='text' name='name' placeholder='type to search...' onChange={(e) =>( 
+        dispatch(filterCoins(e.target.value))
+        )} />
       </div>
       {
          filteredCoins.length === 0 ? <h3>Oops no Coins!!!</h3> : (
