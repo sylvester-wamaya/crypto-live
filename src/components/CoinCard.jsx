3359314663ac7027b48a6d4eaac3f79daadbe78f
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-07-06 23:01:38 +0300

    Fix return back to filtered items issue

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -1,14 +1,16 @@
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from '../styles/coincard.module.css'
 import {AiFillBackward} from 'react-icons/ai'
 import { useNavigate } from 'react-router-dom';
+import { filterCoins } from '../redux/coins/coinsSlice';
 
 
 
 const CoinCard = () => {
   const { selectedCoin, coins } = useSelector((store) => store.coins)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const coinArray = coins.filter((coin)=> coin.uuid === selectedCoin)
   const coin = coinArray[0]
 if(!coin){
@@ -18,9 +20,13 @@ const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
 });
+function handleClick(){
+  navigate('/')
+  dispatch(filterCoins(''))
+}
       return (
         <>
-        <AiFillBackward style={{fontSize:'2rem', color:`${coin.color}`, cursor:'pointer'}} onClick={()=> navigate('/')}/>
+        <AiFillBackward style={{fontSize:'2rem', color:`${coin.color}`, cursor:'pointer'}} onClick={handleClick}/>
         <div style={{backgroundColor: `${coin.color + '50'}` }} className={styles.coin}>
           <div className={styles.icon}>
           <img style={{ width: '30%', height: 'auto' }} src={coin.iconUrl} />
